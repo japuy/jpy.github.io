9a5c8c32f7b3d11d3a9a065006dfb38835ce3af4
japuy <vrmansyah01@gmail.com>
2026-09-10 05:17:04 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 2000,
+  },
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
+  preview: {
+    port: 4173,
+  },
 })
